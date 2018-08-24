21cf6d335bf64e2c630a193e9c64705ad015e865
peternguyen88 <tronghieubk@gmail.com>
2018-08-24 09:42:46 +0700

    Get service worker worked

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAa4rCwsgsFfuKtAtjcRe3tS6cBs0KLsbg',
-  authDomain: 'gmat-zero-to-hero.firebaseapp.com',
-  databaseURL: 'https://gmat-zero-to-hero.firebaseio.com',
-  projectId: 'gmat-zero-to-hero',
-  storageBucket: 'gmat-zero-to-hero.appspot.com',
-  messagingSenderId: '227562885868'
-};
